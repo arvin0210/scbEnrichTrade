98c02d3b221b77a7d577dd4ad0e8508b83635deb
Arvin Pillaai <sp0210@hotmail.com>
2022-06-19 14:54:42 +0800

    upload file size limit to 5MB

--- README.md
+++ README.md
@@ -11,3 +11,7 @@ to run compiled project
 - POST: http://localhost:9191/v1/enrich to map trade orders to product names, returns 'enrich.csv' mapped file. Needs trade.csv as RequestBody
 
 both http request needs product.csv & trade.csv files respectively
+
+## Maximum Uploaded file size
+maximum Upload file size limited to 5MB.
+- a 100,000 records in a CSV file consumes 2.5MB.
--- src/main/java/com/SCB/enrich/ScbEnrichApplication.java
+++ src/main/java/com/SCB/enrich/ScbEnrichApplication.java
@@ -1,18 +1,12 @@
 package com.SCB.enrich;
 
-import com.SCB.enrich.Service.TradingService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import java.io.File;
 
 @SpringBootApplication
 public class ScbEnrichApplication {
 
-	@Autowired
-	private static TradingService service;
-
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) {
 		SpringApplication.run(ScbEnrichApplication.class, args);
 	}
 
